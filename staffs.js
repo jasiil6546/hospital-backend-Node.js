89384c398ca2f773cced00fe59bb32311536d92c
jasiil6546 <m.jasilof@gmail.com>
2024-11-20 06:37:40 +0530

    project

--- staffs.js
+++
@@ -1,8 +0,0 @@
-// var express = require('express')
-// var router = express.Router();
-// router.get('/', function(req, res) {
-//     res.send('Getvrute on things');
-// });
-// router.post('/', function(req, res) {
-//     res.send('postvrute on things');
-// });
